3f6566a4fe5532f2ae766441a1bf79099c16cdd2
Robin73389 <12229@holbertonstudents.com>
2026-04-22 23:02:13 +0200

    Create module environment for stocker the subdirectory in str and I have create file JSON for stock the file stock in modules

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,6 +1,6 @@
 import os
 
 def run(**args):
-print("[*] Dans le module de la liste de diffusion")
-file = os.listdir(".")
-return str(file)
+    print("[*] Dans le module de la liste de diffusion")
+    file = os.listdir(".")
+    return str(file)
---
+++ modules/environment.py
@@ -0,0 +1,6 @@
+import os
+
+
+def run(**args):
+    print("[*] Dans le module d'environnement")
+    return os.environnement

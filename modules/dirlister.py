fb8b544d98369242a60392287410afb25fd6657d
Robin73389 <12229@holbertonstudents.com>
2026-04-29 23:37:53 +0200

    I have create the first prototype program socket for bind two machine distant

--- modules/dirlister.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def run(**args):
-    print("[*] Dans le module de la liste de diffusion")
-    file = os.listdir(".")
-    return str(file)
--- modules/environment.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def run(**args):
-    print("[*] Dans le module d'environnement")
-    return os.environnement
